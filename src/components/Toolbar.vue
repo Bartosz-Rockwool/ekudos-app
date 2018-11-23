bb5ae1cc79235c9733e05fc2b778770a72255d32
Hubert Szczepański <hubert.szczepanski@rockwool.com>
2018-11-23 14:19:00 +0100

    Changed styling to new mockup, moved components to seprate files.

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-toolbar app flat class="white" height="105" >
+        <img src="../assets/rockudo_logo_black.svg" height="75"/>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat href="#kudo">
+                <div class="img-with-text">
+                    <img src="../assets/mainmenu_1.png"/>
+                    <p class="font-weight-black">Give Kudos</p>
+                </div>
+            </v-btn>
+            <v-btn flat href="#kudo-bord">
+                <div class="img-with-text">
+                    <img src="../assets/mainmenu_2.png"/>
+                    <p class="font-weight-black">Kudo Board</p>
+                </div>
+            </v-btn>
+            <v-btn flat href="#kudo-info">
+                <div class="img-with-text">
+                    <img src="../assets/mainmenu_3.png"/>
+                    <p class="font-weight-black">About Kudo</p>
+                </div>                
+            </v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+
+export default {
+    methods: {
+        addKudo(kudosIndex) {
+            console.log(this.$refs)
+            this.$nextTick(() => this.$refs.whom.focus()) 
+        }
+    }
+};
+</script>
+
+<style>
+    .img-with-text {
+        text-align: justify;
+        font-size: 15px;
+        font-weight: bold;
+    }
+
+    .img-with-text img {
+        display: block;
+        margin: 15px auto;
+    }
+</style>
