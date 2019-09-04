de740c22ce63a40b1bddf7c082ddade3355cc148
Bartosz Zaremba <bartosz.zaremba@rockwool.com>
2019-09-04 13:53:37 +0200

    Added images and moving login button

--- src/components/KudoMain.vue
+++ src/components/KudoMain.vue
@@ -10,17 +10,26 @@
     </div>
     <div v-else>
       <section>
-            <v-layout justify-center id="kudo">
+        <v-layout justify-center id="kudo">
         <v-flex xs12 sm8 md6 mt-5>
             <v-card dark class="transparent" flat>
-                <v-card-title class="display-2 font-weight-bold" >
+                <v-card-title class="display-2 font-weight-bold d-flex align-center" >
                     <v-card-text>
                         <p class="text-xs-center">Please log in to use <u class="red-underline">ROCKUDO</u></p>
                     </v-card-text>
+                    <div align="center">
+                        <div cols="12" sm="6">
+                            <div class="text-center">
+                                <div class="my-2">
+                                    <v-btn large color="#d20014" dark v-on:click="login">LOGIN</v-btn>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </v-card-title>
             </v-card>
         </v-flex>
-    </v-layout>
+        </v-layout>
         </section>
     </div>
 </div>
@@ -43,6 +52,11 @@ export default {
   computed: {
     isLoggedUser: function() { return this.$store.getters.isAuthenticated; },
   },
+  methods: {
+    login() {
+        this.$store.dispatch('login');
+    }
+  }
 }
 </script>
 
